9177a80022e0970f70731f1533250c240c37339e
邵钰超 <347504877@qq.com>
2021-02-23 18:57:33 +0800

    ok

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 引入首页样式文件
+// 引入首页样式文件   全局样式
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
